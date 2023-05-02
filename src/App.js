5cbb4893ef8b44a17dc4f1074fb0d06edb082b1f
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-05-02 13:54:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Ross and patrick you're great!</h1>
+    </div>
+  );
 }
 
 export default App;
